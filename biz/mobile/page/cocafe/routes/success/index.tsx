b3a2c1346bd5d32beec04c99a32c0ecf10f8ce26
alienyu <alienyu@foxmail.com>
2021-06-16 18:18:17 +0800

    success

--- biz/mobile/page/cocafe/routes/success/index.tsx
+++ biz/mobile/page/cocafe/routes/success/index.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import { Flex } from 'antd-mobile';
 import { WrappedCmp } from './styled';
-// const successImg = require("@mobileCocafeImgs/success.jpg");
+
+const successImg = require("@mobileCocafeImgs/success.png");
+const mcLogo = require("@mobileCocafeImgs/mcLogo.png");
+const logo = require("@mobileCocafeImgs/Logo.png");
 
 export default class Story extends React.Component<{ history: any }, any> {
     constructor(props: any) {
@@ -14,9 +17,15 @@ export default class Story extends React.Component<{ history: any }, any> {
                 <Flex
                     direction="column" 
                     justify="center" align="center" className="frame">
-                    {/* <img src={successImg} className="successImg" /> */}
+                    <img src={successImg} className="successImg" />
                     <div className="text">已成功提交！</div>
-                    <div className="text"> 感谢你的参与，我们将在铸造完成后通知你</div>
+                    <div className="text"> 感谢你的参与，我们将在</div>
+                    <div className="text">铸造完成后通知你</div>
+                    <div className="adv">共铸焕动回忆</div>
+                    <div className="iconFrame">
+                        <img src={mcLogo} className="mcLogo" />
+                        <img src={logo} className="logo" />
+                    </div>
                 </Flex>
             </WrappedCmp>
         )
--- biz/mobile/page/cocafe/routes/success/styled.ts
+++ biz/mobile/page/cocafe/routes/success/styled.ts
@@ -1,23 +1,56 @@
 import styled from 'styled-components';
 
+const bg = require("@mobileCocafeImgs/bg.png");
+
 const WrappedCmp = styled.div`
     min-height: 100vh;
-    background: #ffcc00;
+    background: url(${bg}) no-repeat;
+    background-size: 100% 100%;
     .frame {
         width: 100%;
         min-height: 100vh;
         .successImg {
-            width: 4.07rem;
-            height: 4.07rem;
-            margin-bottom: 0.4rem
+            width: 2rem;
+            height: 2rem;
+            margin-bottom: 0.48rem;
         }
         .text {
             width: 6.16rem;
-            line-height: 0.78rem;
-            font-weight: 400;
-            font-size: 0.56rem;
-            color: #000;
+            line-height: 0.62rem;
+            font-weight: 500;
+            font-size: 0.32rem;
+            color: #e62474;
             text-align: center;
+        }
+        .adv {
+            width: 105%;
+            height: 1.5rem;
+            background: #e62474;
+            color: #fff;
+            font-weight: 400;
+            font-size: 0.98rem;
+            text-align: center;
+            line-height: 1.5rem;
+            margin-top: 0.8rem;
+            transform: rotate(-10deg);
+        }
+        .iconFrame {
+            width: 100%;
+            margin-top: 0.75rem;
+            text-align: center;
+            .mcLogo {
+                display: inline-block;
+                width: 1.25rem;
+                height: 1.09rem;
+                margin-right: 0.65rem;
+                margin-bottom: 0.15rem;
+            }
+            .logo {
+                display: inline-block;
+                width: 0.87rem;
+                height: 1.4rem;
+                margin-right: 0.65rem;
+            }
         }
     }
 `;
